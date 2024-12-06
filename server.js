32a6fe4151a2f90b69b16619b81e3fdb288f59af
Deepblue-Sky2333 <kikansya0818@hotmail.com>
2024-12-06 22:37:56 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -19,30 +19,29 @@ const wss = new WebSocket.Server({ server });
 let playState = { playing: false, currentTime: 0 }; // 播放状态
 const BUFFER_TIME = 0.2; // 缓冲时间，单位：秒
 
-// 处理 WebSocket 连接
+// WebSocket 连接
 wss.on('connection', (ws) => {
     console.log('WebSocket connection established');
 
-    // 初次连接时同步当前播放状态
+    // 初始化广播当前播放状态
     ws.send(JSON.stringify({
         type: 'playState',
         playing: playState.playing,
         currentTime: playState.currentTime,
-        timestamp: Date.now() // 加入时间戳
+        timestamp: Date.now(), // 初始化时间戳
     }));
 
     ws.on('message', (message) => {
         const data = JSON.parse(message);
 
-        // 如果是播放状态消息
         if (data.type === 'playState') {
             const now = Date.now();
 
-            // 更新播放状态，加入缓冲时间
+            // 更新播放状态
             playState = {
                 playing: data.playing,
                 currentTime: data.currentTime + BUFFER_TIME, // 加入缓冲时间
-                timestamp: now
+                timestamp: now,
             };
 
             // 广播播放状态给其他客户端
@@ -52,7 +51,26 @@ wss.on('connection', (ws) => {
                         type: 'playState',
                         playing: playState.playing,
                         currentTime: playState.currentTime,
-                        timestamp: playState.timestamp // 广播时间戳
+                        timestamp: playState.timestamp,
+                    }));
+                }
+            });
+        } else if (data.type === 'syncRequest') {
+            console.log('Received sync request');
+
+            // 找到剩余时间最长的客户端时间
+            const maxTime = Math.max(...Array.from(wss.clients).map(client => {
+                return client.readyState === WebSocket.OPEN && client.currentTime ? client.currentTime : 0;
+            }));
+
+            console.log(`Broadcasting sync time: ${maxTime}`);
+
+            // 广播同步时间给所有客户端
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        type: 'syncAll',
+                        maxTime,
                     }));
                 }
             });
